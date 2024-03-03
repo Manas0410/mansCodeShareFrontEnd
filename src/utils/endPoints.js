517e496ad3eb1154cbf105c292ad0abc3bef3823
manas0410 <manasshrivastava0410@gmail.com>
2024-03-03 18:49:31 +0530

    instructions added

--- src/utils/endPoints.js
+++ src/utils/endPoints.js
@@ -1,3 +1,7 @@
+//RUN THIS ON BROWSER BEFORE STARTING PROJECT =>  // https://manascodeshare.onrender.com/code
+
+// END POINTS =>
+
 // https://manascodeshare.onrender.com/code/post
 // https://manascodeshare.onrender.com/code/get?urlCode=manas1
 // https://manascodeshare.onrender.com/code/update
